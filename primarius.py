356bdf11584d872b5b8e8325a1d97414db9a2d4d
Honsage <musatov.i.a@edu.mirea.ru>
2026-03-22 00:15:33 +0300

    feat: add start command handling

--- primarius.py
+++ primarius.py
@@ -6,10 +6,23 @@ config = dotenv_values()
 bot = TeleBot(token=config['API_TOKEN'])
 
 
+@bot.message_handler(commands=['start'])
+def handle_start_command(message):
+    chat_id = message.chat.id
+    name = message.chat.first_name
+    bot.send_message(
+        chat_id=chat_id,
+        text=f'Здравствуйте, {name}, я бот Primarius.'
+    )
+
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
+    print(message)
     chat_id = message.chat.id
-    bot.send_message(chat_id=chat_id, text='Привет, я бот Primarius.')
+    bot.send_message(
+        chat_id=chat_id,
+        text='Пока что я не понимаю сообщения :('
+    )
 
 
 bot.polling()
