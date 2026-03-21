cc8ff1e609ce1d5cc29950bc59e333867321c0c7
Honsage <musatov.i.a@edu.mirea.ru>
2026-03-22 00:49:41 +0300

    feat: add newcat command

--- primarius.py
+++ primarius.py
@@ -1,5 +1,6 @@
 from dotenv import dotenv_values
 from telebot import TeleBot
+from cats import get_cat_photo
 
 
 config = dotenv_values()
@@ -14,6 +15,22 @@ def handle_start_command(message):
         chat_id=chat_id,
         text=f'Здравствуйте, {name}, я бот Primarius.'
     )
+
+
+@bot.message_handler(commands=['newcat'])
+def new_cat(message):
+    chat_id = message.chat.id
+    response = get_cat_photo()
+
+    if response['is_succeed']:
+        bot.send_photo(chat_id, response['url'])
+    else:
+        bot.send_message(
+            chat_id=chat_id,
+            text='К сожалению, не получилось загрузить фото кота :('
+        )
+
+
 
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
